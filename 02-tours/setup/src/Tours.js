666820f9a52ce1dc864e355e49c1d135d61dd692
Samuel Rey <samuelrey010@gmail.com>
2021-06-11 14:02:51 -0700

    Display Tours, no interactive components

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Tour from './Tour';
 
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours}) => {
+    return (
+        <section>
+            <div className="title">
+                <h2>Our tours</h2>
+                <div className="underline" />
+            </div>
+            <div>
+                {tours.map((tour) => {
+                    return <Tour key={tour.Id} {...tour}/>;
+                })}
+            </div>
+        </section>
+    )
 };
 
 export default Tours;
