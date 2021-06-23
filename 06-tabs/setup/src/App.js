3f4d5a534f4713e24f7d6eab558952eb2ac953d5
Samuel Rey <samuelrey010@gmail.com>
2021-06-23 09:40:35 -0700

    Basic layout using static data

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -4,12 +4,14 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 function App() {
     const [jobs, setJobs] = useState([]);
+    const [activeJob, setActiveJob] = useState(null);
 
     const fetchJobs = async () => {
         try {
             const resp = await fetch(url);
             const json = await resp.json();
             setJobs(json);
+            setActiveJob(0);
         } catch (err) {
             console.log(err);
         }
@@ -19,7 +21,42 @@ function App() {
         fetchJobs();
     }, []);
 
-    return <h2>tabs project setup</h2>;
+    if (jobs.length === 0 || activeJob === null) {
+        return (
+            <section className="section">
+                <div className="loading">
+                    <h2>loading...</h2>
+                </div>
+            </section>
+        );
+    }
+
+    const { title, dates, duties, company } = jobs[activeJob];
+    return (
+        <section className="section">
+            <div className="title">
+                <h2>Experience</h2>
+                <div className="underline"></div>
+            </div>
+            <div className="job-center">
+                <div className="btn-container"></div>
+                <article className="job-info">
+                    <h3>{title}</h3>
+                    <h4>{company}</h4>
+                    <p className="job-date">{dates}</p>
+                    {duties.map((duty) => {
+                        return (
+                            <div className="job-desc">
+                                <FaAngleDoubleRight className="job-icon" />
+                                <p>{duty}</p>
+                            </div>
+                        );
+                    })}
+                </article>
+            </div>
+            <button className="btn">more info</button>
+        </section>
+    );
 }
 
 export default App;
