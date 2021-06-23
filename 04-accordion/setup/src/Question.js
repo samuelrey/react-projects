4162ea417a14e8f52eed2704683df2860d63667f
Samuel <samuelrey010@gmail.com>
2021-06-22 17:14:34 -0700

    Remove unnecessary named function

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -4,15 +4,16 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({ title, info }) => {
     const [showInfo, setShowInfo] = useState(false);
 
-    const toggleMore = () => {
-        setShowInfo(!showInfo);
-    };
-
     return (
         <article className="question">
             <header>
                 <h4>{title}</h4>
-                <button className="btn" onClick={toggleMore}>
+                <button
+                    className="btn"
+                    onClick={() => {
+                        setShowInfo(!showInfo);
+                    }}
+                >
                     {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
                 </button>
             </header>
