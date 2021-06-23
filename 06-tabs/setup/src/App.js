adb5cf9e93137ba35f072bcc25413d2b32b41b28
Samuel Rey <samuelrey010@gmail.com>
2021-06-23 09:15:21 -0700

    Fetch jobs from API

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>
+    const [jobs, setJobs] = useState([]);
+
+    const fetchJobs = async () => {
+        try {
+            const resp = await fetch(url);
+            const json = await resp.json();
+            setJobs(json);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchJobs();
+    }, []);
+
+    return <h2>tabs project setup</h2>;
 }
 
-export default App
+export default App;
