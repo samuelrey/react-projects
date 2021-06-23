e8be114c5436ff4baf3837e0baa25bb5d0b30e0d
Samuel Rey <samuelrey010@gmail.com>
2021-06-22 22:38:15 -0700

    Create category filter buttons based on data

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,9 +3,11 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
 function App() {
     const [menuItems, setMenuItems] = useState(items);
-    const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState(allCategories);
 
     const filterItems = (category) => {
         if (category === "all") {
@@ -22,7 +24,7 @@ function App() {
                     <h2>our menu</h2>
                     <div className="underline"></div>
                 </div>
-                <Categories filterItems={filterItems} />
+                <Categories categories={categories} filterItems={filterItems} />
                 <Menu menuItems={menuItems} />
             </section>
         </main>
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,40 +1,21 @@
 import React from "react";
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories, filterItems }) => {
     return (
         <div className="btn-container">
-            <button
-                className="filter-btn"
-                onClick={() => {
-                    filterItems("all");
-                }}
-            >
-                all
-            </button>
-            <button
-                className="filter-btn"
-                onClick={() => {
-                    filterItems("breakfast");
-                }}
-            >
-                breakfast
-            </button>
-            <button
-                className="filter-btn"
-                onClick={() => {
-                    filterItems("lunch");
-                }}
-            >
-                lunch
-            </button>
-            <button
-                className="filter-btn"
-                onClick={() => {
-                    filterItems("shakes");
-                }}
-            >
-                shakes
-            </button>
+            {categories.map((category, index) => {
+                return (
+                    <button
+                        key={index}
+                        className="filter-btn"
+                        onClick={() => {
+                            filterItems(category);
+                        }}
+                    >
+                        {category}
+                    </button>
+                );
+            })}
         </div>
     );
 };
