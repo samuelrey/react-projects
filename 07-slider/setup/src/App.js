f9d61d9ea80f8d3536e737300a988c3cc8b24643
Samuel Rey <samuelrey010@gmail.com>
2021-06-27 21:52:01 -0700

    Breakout Review component

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
-import { FaQuoteRight } from "react-icons/fa";
+
+import Review from "./review";
 import data from "./data";
 
 function App() {
@@ -35,7 +36,7 @@ function App() {
             </div>
             <div className="section-center">
                 {people.map((person, index) => {
-                    const { id, image, name, title, quote } = person;
+                    const { id } = person;
 
                     let position = "nextSlide";
                     if (index === activeIndex) {
@@ -47,19 +48,7 @@ function App() {
                         position = "lastSlide";
                     }
 
-                    return (
-                        <article key={id} className={position}>
-                            <img
-                                src={image}
-                                alt={name}
-                                className="person-img"
-                            />
-                            <h4>{name}</h4>
-                            <p className="title">{title}</p>
-                            <p className="text">{quote}</p>
-                            <FaQuoteRight className="icon" />
-                        </article>
-                    );
+                    return <Review key={id} position={position} {...person} />;
                 })}
                 <button
                     className="prev"
---
+++ 07-slider/setup/src/review.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaQuoteRight } from "react-icons/fa";
+
+const Review = ({ image, name, title, quote, position }) => {
+    return (
+        <article className={position}>
+            <img src={image} alt={name} className="person-img" />
+            <h4>{name}</h4>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+        </article>
+    );
+};
+
+export default Review;
